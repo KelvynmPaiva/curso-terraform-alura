fc4c1a6bfcfcc9bf4a31c2c8396e6af0d870b692
Kelvyn Paiva <kelvynmpaiva@gmail.com>
2024-10-09 22:42:24 -0300

    Load Balencer

--- infra/main.tf
+++ infra/main.tf
@@ -33,7 +33,7 @@ resource "aws_key_pair" "chaveSSH" {
 } 
 
 resource "aws_autoscaling_group" "grupo" {
-  availability_zones = [ "${var.regiao_aws}a" ]
+  availability_zones = [ "${var.regiao_aws}a", "${var.regiao_aws}b" ]
   name = var.nomeGrupo
   max_size = var.maximo
   min_size = var.minimo
@@ -42,4 +42,42 @@ resource "aws_autoscaling_group" "grupo" {
     id = aws_launch_template.maquina.id
     version = "$Latest"
   }
+
+  target_group_arns = [aws_lb_target_group.alvoLoadBalencer.arns]
+}
+
+resource "aws_default_subnet" "subnet_1" {
+  availability_zone = [ "${var.regiao_aws}a" ]
+}
+
+resource "aws_default_subnet" "subnet_2" {
+  availability_zone = [ "${var.regiao_aws}b" ]
+}
+
+resource "aws_lb" "loadBalancer" { # Criação do Load Balencer
+  internal = false
+  subnets = [ aws_default_subnet.subnet_1.id, aws_default_subnet.subnet_2.id ]
+}
+
+resource "aws_default_vpc" "default" { # Criação de VPC
+  
+}
+
+resource "aws_lb_target_group" "alvoLoadBalencer" {
+  name = "maquinasAlvo"
+  port = "8000"
+  protocol = "HTTP"
+  vpc_id = aws_default_vpc.default.id
+}
+
+resource "aws_lb_listener" "entradaLoadBalencer" {
+  load_balancer_arn = aws_lb.loadBalancer.arn
+  port = "8000"
+  protocol = "HTTP"
+
+    default_action {
+      type = "forward"
+      target_group_arn = aws_lb_target_group.alvoLoadBalencer.arn
+    }
+
 }
